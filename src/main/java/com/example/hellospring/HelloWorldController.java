978a0c7f4e0c8dcb09cfdfa48df6297138fb5c1d
Hendrik Scholtz <schwarzbaer@gmx.biz>
2023-09-13 16:38:19 +0200

    feat: solve bonus tasks

--- src/main/java/com/example/hellospring/HelloWorldController.java
+++ src/main/java/com/example/hellospring/HelloWorldController.java
@@ -1,13 +1,15 @@
 package com.example.hellospring;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rest")
 public class HelloWorldController {
+
+    private final List<Message> messages = new ArrayList<>();
 
     @GetMapping("/hello")
     public String getHello() {
@@ -17,6 +19,39 @@ public class HelloWorldController {
     @GetMapping("/hello/{text}")
     public String getHello(@PathVariable String text) {
         return "Hello': %s".formatted(text);
+    }
+
+/*
+    @PostMapping("/messages")
+    public String addMessage(@RequestBody String body) {
+        System.out.printf("RequestBody: \"%s\"%n", body);
+        return "Ok";
+    }
+*/
+
+    @PostMapping("/messages")
+    public String addMessage(@RequestParam String id, @RequestParam String name, @RequestParam String message) {
+        //System.out.printf("RequestParams -> id:\"%s\", name:\"%s\", message:\"%s\"%n", id, name, message);
+        Message msg = new Message(id, name, message);
+        messages.add(msg);
+        return "%s added".formatted(msg);
+    }
+
+    @GetMapping("/messages")
+    public List<Message> getMessage() {
+        return messages;
+    }
+
+    @DeleteMapping("/messages/{id}")
+    public String deleteMessage(@PathVariable String id) {
+        for (int i=0; i<messages.size(); i++) {
+            Message message = messages.get(i);
+            if (id.equals(message.id())) {
+                messages.remove(i);
+                return "%s removed".formatted(message);
+            }
+        }
+        return "Can't find message with id \"%s\"".formatted(id);
     }
 
 }
---
+++ src/main/java/com/example/hellospring/Message.java
@@ -0,0 +1,5 @@
+package com.example.hellospring;
+
+public record Message (String id, String name, String message){
+
+}
