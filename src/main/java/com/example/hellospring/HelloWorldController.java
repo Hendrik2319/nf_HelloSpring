4f9767b1de674cbe266ece7d57bfcddec0b6f053
Hendrik Scholtz <schwarzbaer@gmx.biz>
2023-09-14 09:28:59 +0200

    feat: tested which HTTP method can be used for which type of data transfer

--- src/main/java/com/example/hellospring/HelloWorldController.java
+++ src/main/java/com/example/hellospring/HelloWorldController.java
@@ -55,30 +55,64 @@ public class HelloWorldController {
     }
 
 
-
-    @GetMapping("/test")
-    public String testBody_Get(@RequestBody String body) {
-        return testBody(body, "GET");
+    @GetMapping   ("/test") public String testBody_Get   (@RequestBody String body) {
+        return testBody(body, "GET"   );
     }
-    @PostMapping("/test")
-    public String testBody_Post(@RequestBody String body) {
-        return testBody(body, "POST");
+    @PostMapping  ("/test") public String testBody_Post  (@RequestBody String body) {
+        return testBody(body, "POST"  );
     }
-    @PutMapping("/test")
-    public String testBody_Put(@RequestBody String body) {
-        return testBody(body, "PUT");
+    @PutMapping   ("/test") public String testBody_Put   (@RequestBody String body) {
+        return testBody(body, "PUT"   );
     }
-    @DeleteMapping("/test")
-    public String testBody_Delete(@RequestBody String body) {
+    @DeleteMapping("/test") public String testBody_Delete(@RequestBody String body) {
         return testBody(body, "DELETE");
     }
-    @PatchMapping("/test")
-    public String testBody_Patch(@RequestBody String body) {
-        return testBody(body, "PATCH");
-    }
+    @PatchMapping ("/test") public String testBody_Patch (@RequestBody String body) { return testBody(body, "PATCH" ); }
 
     public String testBody(String body, String method) {
-        System.out.printf("%s request: \"%s\"%n", method, body);
-        return "Ok";
+        String response = "%s request: \"%s\"".formatted(method, body);
+        System.out.println(response);
+        return response;
+    }
+
+
+    @GetMapping("/form")
+    public String testForm_Get(@RequestParam String id, @RequestParam String name, @RequestParam String message) {
+        return testForm(id, name, message, "GET");
+    }
+    @PostMapping("/form")
+    public String testForm_Post(@RequestParam String id, @RequestParam String name, @RequestParam String message) {
+        return testForm(id, name, message, "POST");
+    }
+    @PutMapping("/form")
+    public String testForm_Put(@RequestParam String id, @RequestParam String name, @RequestParam String message) {
+        return testForm(id, name, message, "PUT");
+    }
+    @DeleteMapping("/form")
+    public String testForm_Delete(@RequestParam String id, @RequestParam String name, @RequestParam String message) {
+        return testForm(id, name, message, "DELETE");
+    }
+    @PatchMapping("/form")
+    public String testForm_Patch(@RequestParam String id, @RequestParam String name, @RequestParam String message) {
+        return testForm(id, name, message, "PATCH");
+    }
+
+    public String testForm(String id, String name, String message, String method) {
+        String response = "%s request -> id:\"%s\", name:\"%s\", message:\"%s\"".formatted(method, id, name, message);
+        System.out.println(response);
+        return response;
+    }
+
+
+    @GetMapping   ("/json") public String testJSON_Get   (@RequestBody Message message) { return testJSON(message, "GET"   ); }
+    @PostMapping  ("/json") public String testJSON_Post  (@RequestBody Message message) { return testJSON(message, "POST"  ); }
+    @PutMapping   ("/json") public String testJSON_Put   (@RequestBody Message message) { return testJSON(message, "PUT"   ); }
+    @DeleteMapping("/json") public String testJSON_Delete(@RequestBody Message message) { return testJSON(message, "DELETE"); }
+    @PatchMapping ("/json") public String testJSON_Patch (@RequestBody Message message) { return testJSON(message, "PATCH" ); }
+
+    public String testJSON(Message message, String method) {
+        String response = "%s request -> %s".formatted(method, message);
+        System.out.println(response);
+        return response;
     }
 }
